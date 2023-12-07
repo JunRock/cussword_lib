77d9c14705f84ce78be8e5aa1514913387dcf345
leejunseok <wnstjr120422@naver.com>
2023-12-07 16:05:40 +0900

    자바 8 문법 수정

--- src/main/java/io/peaceingaza/filtering/cusswordfilter/WordFiltering1.java
+++ src/main/java/io/peaceingaza/filtering/cusswordfilter/WordFiltering1.java
@@ -1,10 +1,11 @@
 package io.peaceingaza.filtering.cusswordfilter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 public class WordFiltering1 extends HashSet<String> implements CussWord{
     public WordFiltering1(){
-        addAll(List.of(cussWord));
+        addAll(Arrays.asList(cussWord));
     }
     public boolean checkMessage(String content){
         return stream().anyMatch(content::contains);
